31b03e7a66ed325390daa080eae2c5cc6bdf4d66
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-01-29 11:42:38 -0700

    python notes

--- Variables/Notes.py
+++ Variables/Notes.py
@@ -1,1 +1,11 @@
 # Governor-Nephi Bakare, Variable
+
+subject = input("What's your favorite subject: ")
+
+big_number = 6969
+
+name= input("How tall are you: ")
+
+name=("Gov")
+
+print(name, "is 6 feet and 7 inches tall, and really likes", subject)
