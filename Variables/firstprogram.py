7b164a06cb5afc7cb3e81a7326b053c05f449647
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-02-04 15:53:25 -0700

    first program py, practice py, notes c (expressions)

---
+++ Variables/firstprogram.py
@@ -0,0 +1,21 @@
+# Governor-Nephi Bakare, First Program Python
+
+city = "Ikeja"
+
+state = "Lagos"
+
+country = "Nigeria"
+
+tribe1 = "Yoruba"
+
+tribe2 = "Ekoi"
+
+print("I was born in the city", city)
+
+print("Ikeja is one of the main cities in the state", state)
+    
+print("Lagos is the most populated state in the country", country)
+    
+print("My father is from the tribe", tribe1)
+    
+print("However, my mother is from the tribe", tribe2)
--- Variables/practice.py
+++ Variables/practice.py
@@ -1,24 +1,24 @@
 # Governor-Nephi Bakare, Variables Practice - Python
 
-my_name = ("Governor-Nephi Bakare")
+my_name = "Governor-Nephi Bakare"
 
 small_number = 6
 
 big_number = 500
 
-breakfast = ("air")
+breakfast = "air"
 
-my_favorite_color = ("blue")
+my_favorite_color = "blue"
 
-school_name = ("The school's name is UCAS (Utah County Academy of Sciences)")
+school_name = "The school's name is UCAS (Utah County Academy of Sciences)"
 
 the_year = 2025
 
-my_eye_color = ("Dark Brown")
+my_eye_color = "Dark Brown"
 
 my_age = 14
 
-my_favorite_subject = ("World Civilization")
+my_favorite_subject = "World Civilization"
 
 print(my_name)
 print(small_number)
