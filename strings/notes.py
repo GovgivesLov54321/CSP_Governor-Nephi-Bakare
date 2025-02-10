1185372d46899167ff0f7bcbd1490b7c011e2b53
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-02-10 11:49:01 -0700

    strings notes python

---
+++ strings/notes.py
@@ -0,0 +1,11 @@
+# Governor-Nephi Bakare, Strings Notes Python
+
+#string is a data type that holds any info surrounded by quotation marks "" ''
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+name = input("What is your first name?\n").strip().capitalize()
+
+#print(f"Hello {name} My name is Gov, welcome to my program")
+
+print(f"this is your name {name}")
