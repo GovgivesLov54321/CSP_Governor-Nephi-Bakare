66c5cd75ca3a086fca1e173a8920c03ea81d5e0b
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-02-11 11:16:01 -0700

    python string notes

--- strings/notes.py
+++ strings/notes.py
@@ -4,8 +4,18 @@
 
 sentence = "The quick brown fox jumps over the lazy dog"
 
-name = input("What is your first name?\n").strip().capitalize()
+#name = input("What is your first name?\n").strip().upper()
 
 #print(f"Hello {name} My name is Gov, welcome to my program")
 
-print(f"this is your name {name}")
+#print(f"this is your name {name}")
+
+#print("this is your name "+ name)
+
+#print(len(sentence))
+
+#print(sentence[4])
+
+start = (sentence.find("brown"))
+length = len("brown fox")
+print(sentence[start:start+length])
