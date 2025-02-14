9281e0bc5ef177787fde1fb25b09f9d06d287f1a
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-02-14 11:39:20 -0700

    name decorator c

--- strings/name_decorator.c
+++ strings/name_decorator.c
@@ -1,7 +1,17 @@
 // Governor-Nephi Bakare, Name Decorator C
 #include <stdio.h>
 
+char name [1000];
 
 int main(void){
+    printf("What is your name, user? (only one word please): \n");
+    scanf("%s", name);
+    printf("Welcome to my program. I will be decorating your name! \n");
+    
+    
+    char decor1 [20] = ("$$$");
+    char decor2 [20] = ("@@@");
+
+    printf("%s%s%s", decor2, name, decor1);
     return 0;
 }
