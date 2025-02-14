07731c2ed63945bab5f7adcd1ea506bcc2080b53
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-02-14 17:39:29 +0000

    name decorator python

--- strings/name_decorator.py
+++ strings/name_decorator.py
@@ -1,2 +1,11 @@
 # Governor-Nephi Bakare, Name Decorator Python
 
+
+
+name = input("Hello! What's your first name?(one word only please): \n")
+
+decor1 = (";p;O;p")
+
+decor2 = ("<3<3<3")
+
+print(decor2 + name + decor1)
