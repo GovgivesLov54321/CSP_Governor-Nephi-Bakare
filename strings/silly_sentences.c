dc7e3c89a874d14a047dbca7e7eee01f238cea39
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-02-21 11:24:12 -0700

    silly sentences for c

--- strings/silly_sentences.c
+++ strings/silly_sentences.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 // empty string variables for use words (minimum 3)
 char character[100];
-char location[100]
+char location[100];
+char verb[100];
 
 int main(void){
     // A welcome for the user telling them what the program is (print)
@@ -11,7 +12,12 @@ int main(void){
     //ask user for words (print statement with a question scanf to set to a variable) (in C we need to tell the user that they can only type one word)
      printf("What is your favorite Disney character? (one word only please):\n");
      scanf("%s", character);
-     printf("Type in the most random place you can think of. (one word only please):");
+     printf("Type in the most random place you can think of. (one word only please):\n");
+     scanf("%s", location);
+     printf("Type in a random action that character is doing (one word only please):\n");
+     scanf("%s", verb);
+
     // print out the story with the variables inserted ("welcome %s to my program", variable) 
+    printf("In a far, far place, %s was %s with happiness in %s", character, verb, location);
     return 0;
 }
