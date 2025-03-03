be4669256a16754c28ec705c9a7db36ea039329d
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-03-03 11:49:05 -0700

    loops notes python

---
+++ loops/notes.c
@@ -0,0 +1,9 @@
+// Governor-Nephi Bakare, Loops Notes C
+#include <stdio.h>
+//How do you make lists in C?
+//How do you make for loops in C?
+//How do you make while loops in C?
+
+int main(void){
+    return 0;
+}
