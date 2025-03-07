eb38b1766b7709f4f3b68d480d06e6583dcf159c
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-03-07 11:49:20 -0700

    financial calc update c and python

--- functions/financial_calc_update.c
+++ functions/financial_calc_update.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 #include <math.h>
 
+char name [1000];
+float user_input(char*pregunta){
+    float value;
+    printfi("%s", pregunta);
+    scnaf("%f", &value);
+    return value;
+}
+void print_result(char*item_name, float numero, float income){
+    float percentage = (numero/income)*100;
+    printf("Your monthly %s is $%.2f which is %.2f%% of your income!\n", item_name, numero, income);
+}
 int main(void){
+    float income, rent, utilities, groceries, transportation, savings, spendings; 
+    printf("Hello! What is your name? (please only answer with one word): \n");
+    scanf("%s", name);
+    printf("Welcome to my program, %s! This program will calculate the percentage of your spendings of your income. \n", name);
+    income = userinput("What is your income?: \n");
+    rent = userinput("What is your rent?: \n");
+    utilities = userinput("How much do your utilities cost?: \n");
+    groceries = userinput("How much do your groceries cost?: \n");
+    transportation = userinput("How much does transportation cost (gas, bus pass, Lyft, etc.)?: \n");
+    savings = income*.1;
+    spendings = income-savings-rent-utilities-groceries-transportation;
+
+    print_result("rent", rent, income);
+    
     return 0;
 }
--- functions/financial_calc_update.py
+++ functions/financial_calc_update.py
@@ -1,32 +1,22 @@
 # Governor-Nephi Bakare, Financial Calculator Update Python
 def info(cost, income, type):
-    rent_percentage = cost/income*100
-    print(f"Your {type} is ${cost:.2f}, which is {rent_percentage} percent of your income.")
-
-# print welcome statement that welcomes user and tells what program does
+    percentage = cost/income*100
+    print(f"Your {type} is ${cost:.2f}, which is {percentage} percent of your income.")
 
 name = input("What is your first and last name?\n")
 
 print(f"Welcome to my progam, {name}. This program will calculate the percentage of your spendings of your income!\n")
 
-def info(answer):
-    pregunta = (f"How much does your {answer} cost you monthly?")
+def user(answer):
+    pregunta = (f"How much does your {answer} cost you monthly?\n")
     return float(input(pregunta))
 
-
-# ask what their income is (variable an input)
-income = float(input("What is your income?\n"))
-# ask what their rent is (variable an input)
-rent = float(input("What is your rent?\n"))
-# ask what their utilities is (variable an input)
-utilities = float(input("How much do your utilities cost?\n"))
-# ask what their groceries is (variable an input)
-groceries = float(input("How much do your groceries cost?\n"))
-# ask what their transportation is (variable an input)
-transportation = float(input("How much does transporation cost?\n"))
-# calculate savings as 10% of income (income*.1)
+income = user("income")
+rent = user("rent")
+utilities = user("utilities")
+groceries = user("groceries")
+transportation = user("transportation")
 savings = income*.1
-# calculate spending as income-savings-rent-utilities-groceries-transportation (variable)
 spending = income-savings-rent-utilities-groceries-transportation
 
 info(rent, income, "rent")
@@ -35,5 +25,3 @@ info(groceries, income, "groceries")
 info(transportation, income, "transportation")
 info(savings, income, "savings")
 info(spending, income, "spending")
-
-info()
