2a4649399c26f444b69b9d3a55ee75368a2eb096
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-03-17 17:04:11 +0000

    financial calc update c

--- functions/financial_calc_update.c
+++ functions/financial_calc_update.c
@@ -2,31 +2,38 @@
 #include <stdio.h>
 #include <math.h>
 
-char name [1000];
 float user_input(char*pregunta){
     float value;
-    printfi("%s", pregunta);
-    scnaf("%f", &value);
+    printf("%s", pregunta);
+    scanf("%f", &value);
     return value;
 }
 void print_result(char*item_name, float numero, float income){
     float percentage = (numero/income)*100;
-    printf("Your monthly %s is $%.2f which is %.2f%% of your income!\n", item_name, numero, income);
+    printf("Your monthly %s is $%.2f which is %.2f%% of your income!\n", item_name, numero, percentage);
 }
-int main(void){
-    float income, rent, utilities, groceries, transportation, savings, spendings; 
+int main(void){ 
     printf("Hello! What is your name? (please only answer with one word): \n");
     scanf("%s", name);
     printf("Welcome to my program, %s! This program will calculate the percentage of your spendings of your income. \n", name);
-    income = userinput("What is your income?: \n");
-    rent = userinput("What is your rent?: \n");
-    utilities = userinput("How much do your utilities cost?: \n");
-    groceries = userinput("How much do your groceries cost?: \n");
-    transportation = userinput("How much does transportation cost (gas, bus pass, Lyft, etc.)?: \n");
-    savings = income*.1;
+    
+    float income, rent, utilities, groceries, transportation, savings, spendings;
+
+    income = user_input("What is your income?: \n");
+    rent = user_input("What is your rent?: \n");
+    utilities = user_input("How much do your utilities cost?: \n");
+    groceries = user_input("How much do your groceries cost?: \n");
+    transportation = user_input("How much does transportation cost (gas, bus pass, Lyft, etc.)?: \n");
+    
+    savings = income*0.1;
     spendings = income-savings-rent-utilities-groceries-transportation;
 
     print_result("rent", rent, income);
+    print_result("utilities", utilities, income);
+    print_result("groceries", groceries, income);
+    print_result("transportation", transportation, income);
+    print_result("savings", savings, income);
+    print_result("spendings", spendings, income);
     
     return 0;
 }
