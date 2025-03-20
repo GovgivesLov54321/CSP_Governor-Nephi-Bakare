eed895dbb0e348a4c339ab2aa9ebcf016d8997d9
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-03-20 11:49:17 -0600

    my family looops c and python

---
+++ loops/my_family_loop.c
@@ -0,0 +1,9 @@
+// Governor-Nephi Bakare
+#include <stdio.h>
+
+
+int main(void){
+    char hermanitos[][1000] = {"Kehinde, Taiwo, Idowu"};
+    printf("Hello there, %s!\n", hermanitos []);
+    return 0;
+}
