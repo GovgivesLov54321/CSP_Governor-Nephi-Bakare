de8a99f726eb323f5263ded99547357062cb608a
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-03-21 11:15:59 -0600

    my family loop c completed

--- loops/my_family_loop.c
+++ loops/my_family_loop.c
@@ -1,9 +1,15 @@
 // Governor-Nephi Bakare
 #include <stdio.h>
 
+ char hermanitos[][1000] = {"Kehinde", "Taiwo", "Idowu"};
 
 int main(void){
-    char hermanitos[][1000] = {"Kehinde, Taiwo, Idowu"};
-    printf("Hello there, %s!\n", hermanitos [0]);
+   int flength = sizeof(hermanitos)/sizeof(hermanitos[0]);
+    
+    int f;
+    for (f=0;f<flength;f++){
+        printf("Hello there, %s!\n", hermanitos[f]);
+    }
+
     return 0;
 }
---
+++ loops/my_family_loop.py
@@ -0,0 +1,4 @@
+# Governor-Nephi Bakare, My Family Loop Python
+
+primos = ["Ewaoluwa", "Daracemi", "Fola"]
+
