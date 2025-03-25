ab1d7f17a1afe340617a696dd449b0db32eb9d01
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-03-25 16:42:26 -0600

    build a game python

---
+++ programming_final/build_a_game.py
@@ -0,0 +1,30 @@
+# Governor-Nephi Bakare, Hangman Python
+print("Welcome to hangman!")
+print("______________")
+print("|            |")
+print("|             ")
+print("|             ")
+print("|             ")
+print("[][][]        \n")
+
+
+#Brahm: loop/ which word(in a list) to make the user guess
+
+random_words = ["software", "iconic", "dirtbikes", "manager", "pepsi", "explode", "investagator", "exciting", "attitude", "computer", "cyber", "architect", "printer", "publication", "evaluation", "rhinoceros", "pharoah", "crocodile", "alligator", "pneumonoultramicroscopicsilicovolcanoconiosis"]
+
+#for random_word in random_words:
+print (random_words[0-20])
+
+
+#Kaleb: function that runs if they get the letter wrong (includes making the hangman if wrong)
+#def wrd(wrong):
+    #(input(f"What is your {wrong}?\n"))
+
+
+#Gov: the other function that runs when the user gets it correct (includes spelling the word at the bottom)
+#def letter(right):
+    #(input(f"What is your {right}?\n"))
+
+
+
+#Zoe: conditional runs when the user fails or wins then repeats the code
