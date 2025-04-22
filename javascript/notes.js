7874e9952eef253606fc19cb5c8c8d2c3de8f56e
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-04-22 11:47:24 -0600

    javascript notes

---
+++ javascript/notes.js
@@ -0,0 +1,10 @@
+//function hello(){
+  //  document.getElementById("title").style.color = "green"
+//}
+let images = ["https://www.si.com/.image/t_share/MTY4MjYxNjYzMzI4OTY5ODkz/ali78101622jpg.jpg", "https://photographyproject.uk/wp-content/uploads/2022/01/23-2103-post/Ali-vs-Liston.jpg", "https://ichef.bbci.co.uk/ace/standard/624/cpsprodpb/6E08/production/_89886182_ali_getty.jpg"]
+
+function change(){
+    i (counter < images.length){
+        document.getElementsByClassName("img").src = images(counter)
+    }
+}
