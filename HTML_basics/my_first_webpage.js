7fcb6706a96fd6addf8b46115e1b17d8ae89576b
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-04-23 11:49:08 -0600

    stuff

---
+++ HTML_basics/my_first_webpage.js
@@ -0,0 +1,5 @@
+// Hawks: Trae Young = https://fadeawayworld.net/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTk4NDgwNzY3NjA3NjQ2MTE5/usatsi_20538503.webp
+// Celtics: Jayson Tatum = https://fadeawayworld.net/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTk1MDg3Mjc1ODI5MjQxNTcw/usatsi_19732023.webp
+// Nets: Cam Thomas = https://fadeawayworld.net/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MTkzNDY0MTE0NTQwOTc5OTAx/usatsi_19261594.webp
+// Hornets: Lamelo Ball = https://fadeawayworld.net/.image/c_limit%2Ccs_srgb%2Cq_auto:good%2Cw_700/MjAzOTgwNjMxNjA3MDkyNzMx/usatsi_22365299.webp
+//site for all players = https://fadeawayworld.net/nba/every-nba-teams-best-player-for-the-2024-25-season
