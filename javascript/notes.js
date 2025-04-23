129176e6af1354c5d45b5cc86c8824fa3fabff0e
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-04-23 11:36:37 -0600

    structure your site

--- javascript/notes.js
+++ javascript/notes.js
@@ -3,8 +3,18 @@
 //}
 let images = ["https://www.si.com/.image/t_share/MTY4MjYxNjYzMzI4OTY5ODkz/ali78101622jpg.jpg", "https://photographyproject.uk/wp-content/uploads/2022/01/23-2103-post/Ali-vs-Liston.jpg", "https://ichef.bbci.co.uk/ace/standard/624/cpsprodpb/6E08/production/_89886182_ali_getty.jpg"]
 
+function hello(){
+  let name = window.prompt("What's your name?")
+  document.getElementById("title").innerHTML = "Hello0" + name + ":)"
+}
 function change(){
     i (counter < images.length){
         document.getElementsByClassName("img").src = images(counter)
     }
+}
+function hover(){
+  document.getElementById("title").inner
+}
+function pop(){
+  window.alert("Really, don't click that...")
 }
