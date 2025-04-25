59e33ade185962647a09b803e3259fe632f2cac6
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2025-04-25 11:49:32 -0600

    my first webpage

--- HTML_basics/my_first_webpage.js
+++ HTML_basics/my_first_webpage.js
@@ -2,5 +2,26 @@
 // Milwaukee Bucks: Giannis Antetokounmpo = https://img.buzzfeed.com/buzzfeed-static/static/2021-04/26/19/asset/e498764414c5/anigif_sub-buzz-248-1619464041-1.gif
 // Boston Celtics: Jayson Tatum = https://upload.wikimedia.org/wikipedia/commons/1/1e/Jayson_Tatum_%2851839569977%29_%28cropped%29.jpg
 // Oklahoma City Thunder: Shai Gilgeous-Alexander = https://kfor.com/wp-content/uploads/sites/3/2022/11/2022-OKC-Mil-SGA.jpg?w=612
-// Los Angeles Lakers: LeBron James = https://athlonsports.com/.image/t_share/MjE0MjU2ODMxMDI1OTE1Njg0/usatsi_25908376.jpg
-let images = ["https://static01.nyt.com/athletic/uploads/wp/2023/10/07085025/231007-Nikola-Jokic-scaled-e1696683052376.jpg", "https://kfor.com/wp-content/uploads/sites/3/2022/11/2022-OKC-Mil-SGA.jpg?w=612", "https://img.buzzfeed.com/buzzfeed-static/static/2021-04/26/19/asset/e498764414c5/anigif_sub-buzz-248-1619464041-1.gif", "https://upload.wikimedia.org/wikipedia/commons/1/1e/Jayson_Tatum_%2851839569977%29_%28cropped%29.jpg", "https://athlonsports.com/.image/t_share/MjE0MjU2ODMxMDI1OTE1Njg0/usatsi_25908376.jpg"]
+// Los Angeles Lakers: Luka Doncic = https://cdn.nba.com/teams/uploads/sites/1610612747/2025/04/2425_lal_highlight_thumb_250419_luka_2000.jpg
+let photograph = ["https://static01.nyt.com/athletic/uploads/wp/2023/10/07085025/231007-Nikola-Jokic-scaled-e1696683052376.jpg", "https://kfor.com/wp-content/uploads/sites/3/2022/11/2022-OKC-Mil-SGA.jpg?w=612", "https://img.buzzfeed.com/buzzfeed-static/static/2021-04/26/19/asset/e498764414c5/anigif_sub-buzz-248-1619464041-1.gif", "https://upload.wikimedia.org/wikipedia/commons/1/1e/Jayson_Tatum_%2851839569977%29_%28cropped%29.jpg", "https://cdn.nba.com/teams/uploads/sites/1610612747/2025/04/2425_lal_highlight_thumb_250419_luka_2000.jpg"]
+
+
+let counter = 0
+
+function change(){
+    if (counter < photograph.length){
+        document.getElementById("photograph").src=photograph[counter] 
+        counter +=1
+    }
+    else{
+        counter=0
+        document.getElementById("photograph").src=photograph[counter]
+    }
+}
+function hover(){
+    document.getElementById("photograph").src = "https://cdn.britannica.com/07/5207-050-5BC9F251/Gray-wolf.jpg"
+}
+
+function leave(){
+    document.getElementById("photograph").src = "https://www.pbs.org/wnet/nature/files/2014/10/HoneyBadger-Main-e1415392112925-1280x720.jpg"
+}
--- javascript/notes.js
+++ javascript/notes.js
@@ -1,20 +1,44 @@
 //function hello(){
-  //  document.getElementById("title").style.color = "green"
+  //  document.getElementById("title").style.color = "orange"
 //}
-let images = ["https://www.si.com/.image/t_share/MTY4MjYxNjYzMzI4OTY5ODkz/ali78101622jpg.jpg", "https://photographyproject.uk/wp-content/uploads/2022/01/23-2103-post/Ali-vs-Liston.jpg", "https://ichef.bbci.co.uk/ace/standard/624/cpsprodpb/6E08/production/_89886182_ali_getty.jpg"]
+let images = ["https://static01.nyt.com/athletic/uploads/wp/2023/10/07085025/231007-Nikola-Jokic-scaled-e1696683052376.jpg", "https://kfor.com/wp-content/uploads/sites/3/2022/11/2022-OKC-Mil-SGA.jpg?w=612", "https://img.buzzfeed.com/buzzfeed-static/static/2021-04/26/19/asset/e498764414c5/anigif_sub-buzz-248-1619464041-1.gif", "https://upload.wikimedia.org/wikipedia/commons/1/1e/Jayson_Tatum_%2851839569977%29_%28cropped%29.jpg", "https://cdn.nba.com/teams/uploads/sites/1610612747/2025/04/2425_lal_highlight_thumb_250419_luka_2000.jpg"]
 
-function hello(){
-  let name = window.prompt("What's your name?")
-  document.getElementById("title").innerHTML = "Hello0" + name + ":)"
-}
+let counter = 0
+
 function change(){
-    i (counter < images.length){
-        document.getElementsByClassName("img").src = images(counter)
+    if (counter < images.length){
+        document.getElementById("img").src=images[counter] 
+        counter +=1
+    }
+    else{
+        counter=0
+        document.getElementById("img").src=images[counter]
     }
 }
-function hover(){
-  document.getElementById("title").inner
+
+//function hello(){
+//    document.getElementById("title").innerHTML = "Hello World!"
+//}
+
+function hello(){
+    let name = window.prompt("What is your name? ", "Enter text here")
+    document.getElementById("title").innerHTML = "Hello, " + name + "!"
 }
+
+
+
+function hover(){
+    document.getElementById("img").src = "https://cdn.britannica.com/07/5207-050-5BC9F251/Gray-wolf.jpg"
+}
+
+function leave(){
+    document.getElementById("img").src = "https://www.pbs.org/wnet/nature/files/2014/10/HoneyBadger-Main-e1415392112925-1280x720.jpg"
+}
+
+function show(){
+    document.getElementById("lost").style.display = "block"
+}
+//You cannot unhide this by calling it on the image. You have to attach it somewhere else.
 function pop(){
-  window.alert("Really, don't click that...")
+    window.alert("Really, don't click that.")
 }
